55c1b12365621f3ac22b73c291e4564669ba275e
Dhruvil Dobariya <190540107051@darshan.ac.in>
2022-03-23 08:41:41 +0530

    Complate user business access layer

--- App_Code/BAL/UserBALBase.cs
+++ App_Code/BAL/UserBALBase.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AddressBook.DAL;
+using AddressBook.ENT;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Web;
 
@@ -11,11 +14,67 @@ namespace AddressBook.BAL
 {
     public abstract class UserBALBase
     {
-        public UserBALBase()
+        protected string _Message;
+        public string Message
         {
-            //
-            // TODO: Add constructor logic here
-            //
+            get { return _Message; }
+            set { _Message = value; }
         }
+
+        #region Validate User
+        public UserENT ValidateUser(SqlString UserName, SqlString Password)
+        {
+            UserDAL userDAL = new UserDAL();
+            return userDAL.ValidateUser(UserName, Password);
+        }
+        #endregion Validate User
+
+        #region Insert
+        public bool Insert(UserENT entUser)
+        {
+            UserDAL userDAL = new UserDAL();
+            if (userDAL.Insert(entUser))
+            {
+                return true;
+            }
+            else
+            {
+                _Message = userDAL.Message;
+                return false;
+            }
+        }
+        #endregion Insert
+
+        #region Update
+        public bool Update(UserENT entUser)
+        {
+            UserDAL userDAL = new UserDAL();
+            if (userDAL.Update(entUser))
+            {
+                return true;
+            }
+            else
+            {
+                _Message = userDAL.Message;
+                return false;
+            }
+        }
+        #endregion Update
+
+        #region Delete
+        public bool Delete(SqlInt32 UserId)
+        {
+            UserDAL userDAL = new UserDAL();
+            if (userDAL.Delete(UserId))
+            {
+                return true;
+            }
+            else
+            {
+                _Message = userDAL.Message;
+                return false;
+            }
+        }
+        #endregion Delete
     }
 }
